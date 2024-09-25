4fab115ebce7538ffc4eb40e0fec1230597b015e
Kartik <narkhede.1@iitj.ac.in>
2024-09-25 20:06:16 +0530

    Your commit message here

---
+++ src/controllers/userController.js
@@ -0,0 +1,316 @@
+import { request } from 'graphql-request';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const getUserData = async (req, res) => {
+    const LEETCODE_API_URL = process.env.LEETCODE_API_URL;
+    const username = req.params.username;  // Get username from URL parameters
+    const query = `#graphql
+      query getUserProfile($username: String!) {
+        matchedUser(username: $username) {
+          username
+          profile {
+            realName
+            userAvatar
+            reputation
+            ranking
+           
+          }
+          githubUrl
+          twitterUrl
+          linkedinUrl
+
+          languageProblemCount {
+            languageName
+            problemsSolved
+          }
+        }
+      }
+    `;
+
+    try {
+      const variables = { username };
+      const data = await request(LEETCODE_API_URL, query, variables);
+      const userData = data.matchedUser;
+      console.log(userData);
+
+      return res.status(200).json({
+        username: userData.username,
+        profile: userData.profile,
+        githubUrl: userData.githubUrl,
+        twitterUrl: userData.twitterUrl,
+        linkedinUrl: userData.linkedinUrl,
+        languageProblemCount: userData.languageProblemCount,
+    });
+    } catch (error) {
+        return res.status(error.response.status).json({
+            message :error.response.errors
+        });
+    }
+};
+
+export const getRecentSubmissionData = async (req,res)=>{
+  const LEETCODE_API_URL= process.env.LEETCODE_API_URL;
+  const username= req.params.username;
+  const query=`#graphql
+    query getRecentSubmission($username:String!){
+      recentAcSubmissionList(username:$username){
+        id
+        title
+      }
+    }
+  `
+  try{
+    const variables= {username};
+    const submissionData=await request(LEETCODE_API_URL,query,variables);
+    console.log(submissionData);
+    return res.status(200).json({
+      submissionData
+    })
+  }catch(error) {
+    return res.status(error.response.status).json({
+        message :error.response.errors
+    });
+}
+};
+
+export const getDataForSubmissionStat= async (req,res)=>{
+  const LEETCODE_API_URL = process.env.LEETCODE_API_URL;
+  const username = req.params.username;
+
+  const query = `#graphql
+    query dataForSubmissionStats($username: String!) {
+      allQuestionsCount{
+        difficulty
+        count
+      }
+      matchedUser(username: $username) {
+        submitStats{
+          acSubmissionNum{
+            difficulty
+            count
+            submissions
+          }
+          totalSubmissionNum{
+            difficulty
+            count
+            submissions
+          }
+        }
+      }
+    }
+  `;
+
+  try {
+    const variables = { username };
+    const dataForSubmissionStats = await request(LEETCODE_API_URL, query, variables);
+    // Returning both badges and upcomingBadges in the response
+    return res.status(200).json({
+      dataForSubmissionStats
+    });
+  }catch(error){
+    return res.status(error.response.status).json({
+        message :error.response.errors
+    });
+  }
+}
+
+
+//query not working
+// export const getContestRatingHistogramData = async (req, res) => {
+//   const LEETCODE_API_URL = process.env.LEETCODE_API_URL;
+//   const username = req.params.username;
+
+//   const query = `#graphql
+//     query getContestRatingHistogram($username: String!) {
+//       contestRatingHistogram(username: $username) { 
+//         userCount
+//         ratingStart
+//         ratingEnd
+//         topPercentage
+//       }
+//     }
+//   `;
+
+//   try {
+//     const variables = { username };
+//     const contestRatingHistogramData = await request(LEETCODE_API_URL, query, variables);
+//     console.log(contestRatingHistogramData);
+
+//     // Return response with contest rating histogram data
+//     return res.status(200).json({
+//       contestRatingHistogramData,
+//     });
+//   } catch (error) {
+//     return res.status(error.response.status).json({
+//         message :error.response.errors
+//     });
+// }
+// };
+
+export const getBadgeData = async (req, res) => {
+  const LEETCODE_API_URL = process.env.LEETCODE_API_URL;
+  const username = req.params.username;
+
+  const query = `#graphql
+    query badgeData($username: String!) {
+      matchedUser(username: $username) {
+        badges {
+          id
+          name
+          shortName
+          displayName
+          icon
+          hoverText
+          medal {
+            slug
+            config {
+              iconGif
+              iconGifBackground
+            }
+          }
+          creationDate
+          category
+        }
+        upcomingBadges {
+          name
+          icon
+          progress
+        }
+      }
+    }
+  `;
+
+  try {
+    const variables = { username };
+    const badgeData = await request(LEETCODE_API_URL, query, variables);
+    
+    console.log(badgeData);
+
+    // Returning both badges and upcomingBadges in the response
+    return res.status(200).json({
+      badges: badgeData.matchedUser.badges,
+      upcomingBadges: badgeData.matchedUser.upcomingBadges
+    });
+  }catch(error){
+    return res.status(error.response.status).json({
+        message :error.response.errors
+    });
+  }
+}
+
+export const getActiveBadgeData=async (req,res)=>{
+  const LEETCODE_API_URL=process.env.LEETCODE_API_URL;
+  const username =req.params.username;
+
+  const query = `#graphql 
+    query getActiveBadge($username : String!){
+      matchedUser(username:$username){
+        activeBadge{
+          displayName
+          icon
+        }
+      }
+    }
+  `
+
+  try {
+    const variables={username};
+    const badgeData= await request(LEETCODE_API_URL,query,variables);
+    return res.status(200).json({
+      badgeData
+    })
+  }catch(error){
+    return res.status(error.response.status).json({
+      message :error.response.errors
+  });
+  }
+};
+export const getProblemCountDataByTag = async (req, res) => {
+  const LEETCODE_API_URL = process.env.LEETCODE_API_URL;
+  const username = req.params.username;  // Get username from URL parameters
+  const query = `#graphql
+    query getProblemCountByTag($username: String!) {
+      matchedUser(username: $username) {
+        tagProblemCounts{
+          advanced{
+            tagName
+            problemsSolved
+          }
+          intermediate{
+            tagName
+            problemsSolved
+          }
+          fundamental{
+            tagName
+            problemsSolved
+          }
+
+        }  
+      }
+    }
+  `;
+
+  try {
+    const variables = { username };
+    const data = await request(LEETCODE_API_URL, query, variables);
+    const solvedData = data.matchedUser;
+    return res.status(200).json({
+      solvedData
+  });
+  } catch (error) {
+      return res.status(error.response.status).json({
+          message :error.response.errors
+      });
+  }
+};
+
+
+export const getContestRankingAndHistry = async (req,res) =>{
+  const LEETCODE_API_URL = process.env.LEETCODE_API_URL;
+  const username = req.params.username;  // Get username from URL parameters
+
+  const query = `#graphql 
+    query contestRatingAndHistory($username: String!) {
+    userContestRanking(username: $username) {
+      attendedContestsCount
+      rating
+      globalRanking
+      totalParticipants
+      topPercentage
+      badge {
+        name
+        icon
+      }
+    }
+    userContestRankingHistory(username: $username) {
+      attended
+      trendDirection
+      problemsSolved
+      totalProblems
+      finishTimeInSeconds
+      rating
+      ranking
+      contest {
+        title
+        startTime
+      }
+    }
+  }
+
+  `
+
+  try{
+    const variables={username};
+    const contestData= await request(LEETCODE_API_URL,query,variables);
+    console.log(contestData);
+
+    return res.status(200).json({
+      contestData
+    })
+  }catch(error){
+    return res.status(error.response.status).json({
+      message :error.response.errors
+  });
+}
+};
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import { getProblemCountDataByTag, getUserData ,getRecentSubmissionData, getActiveBadgeData, getBadgeData, getDataForSubmissionStat, getContestRankingAndHistry} from './controllers/userController.js';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Use CORS middleware
+app.use(cors());
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Use user routes
+app.use('/api/userData/:username', getUserData); 
+app.use('/api/solvedCntByTag/:username',getProblemCountDataByTag);
+app.use('/api/recentSubmissions/:username',getRecentSubmissionData);
+app.use('/api/activeBadges/:username',getActiveBadgeData);
+// app.use('/api/contestRatingHistogram/:username',getContestRatingHistogramData);   // not working
+app.use('/api/badgeData/:username',getBadgeData); 
+app.use('/api/dataForSubmissionStats/:username',getDataForSubmissionStat); 
+app.use('/api/contestRankingAndHistry/:username',getContestRankingAndHistry);
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
