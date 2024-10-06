1bd41a332fbbfe7f27cc1f10cdbc4513314df46d
Kartik <narkhede.1@iitj.ac.in>
2024-10-07 01:02:43 +0530

    added helmet , rate limiter ,compression - mdn deployment express

--- src/index.js
+++ src/index.js
@@ -2,12 +2,38 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import { welcomeFunction } from './controllers/userController.js';
-
 import userRoute from "./routes/userRoutes.js"
+
+//newly added
+import  compression from "compression";
+import  helmet from "helmet";
+import RateLimit from "express-rate-limit";
+
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+// Set up rate limiter: maximum of twenty requests per minute
+
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+app.use(compression()); // Compress all routes
+
+
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+      },
+    }),
+  );
 
 // Use CORS middleware
 app.use(cors());
